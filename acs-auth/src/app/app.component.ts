0f8d222a2962e65f8e61bd64d5d43d12c4f44448
Alex C. Souza <alexdcesouza@gmail.com>
2019-08-03 13:27:50 -0300

    First adaptations to angular version of template

---
+++ acs-auth/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'
+],
+
+})
+export class AppComponent {
+  title = 'acs-auth';
+}
